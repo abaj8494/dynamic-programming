e71bc6d1d36fdede46ec503a3188a718566085b0
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-04-27 18:57:09 +1000

    init, fibo

---
+++ README.md
@@ -0,0 +1,8 @@
+# dp 😩
+
+just a practise repo for the powerful but occasionally confusing dynamic-programming paradigm.
+
+# references
+
+- https://www.youtube.com/watch?v=oBt53YbR9Kk
+
---
+++ fib.js
@@ -0,0 +1,16 @@
+function expFib(n) {
+    if (n <= 2) return 1;
+    return expFib(n-1) + expFib(n-2);
+}
+
+function dynamicFib(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = dynamicFib(n-1, memo) + dynamicFib(n-2, memo);
+    return memo[n]
+}
+
+console.log(expFib(5));
+console.log(expFib(6));
+console.log(expFib(7));
+console.log(dynamicFib(50));
